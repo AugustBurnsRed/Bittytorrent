a69c3ad5196175fb0fcd0f069f50766d5540a2e5
Steven Paquet <steven.paquet1@gmail.com>
2015-04-02 14:20:26 -0400

    update menu script

--- themes/asset/js/global.js
+++ themes/asset/js/global.js
@@ -1,10 +1,27 @@
+var baseurl = 'http://localhost/Bittytorrent/';
+
+/*some global function*/
 $(document).ready(function() {
     var url = window.location;
-    // Will only work if string in href matches with location
     $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
 
-    // Will also work for relative and absolute hrefs
+    var pageName = "" 
+    pageName = thisPage();
+
     $('ul.nav a').filter(function() {
-        return this.href == url;
-    }).parent().addClass('active');
+        pageName = baseurl + pageName
+        $('ul.nav a[href="'+ pageName +'"]').parent().addClass('active');
+    });
+
+
 });
+
+function thisPage(){
+    pageName = window.location.href.split(baseurl);
+    pageName = pageName[1].split('/');
+    pageName = pageName[0];
+
+    return pageName;
+}
+
+
