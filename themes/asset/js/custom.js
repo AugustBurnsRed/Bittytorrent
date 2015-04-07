1ba01785f4cb1f4c036ff02118dd8a10c46168b5
Steven Paquet <steven.paquet1@gmail.com>
2015-04-07 15:52:35 -0400

    update menu script + custom php

--- themes/asset/js/custom.js
+++ themes/asset/js/custom.js
@@ -1,23 +1,25 @@
-var baseurl = 'http://localhost/Bittytorrent/';
-
-/*some global function*/
+/*some custom function*/
 $(document).ready(function() {
+
+    /*set the active state on the menu*/
+    var baseurl = $("input#baseurl").val() + '/';
     var url = window.location;
+
     $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
 
-    var pageName = "" 
-    pageName = thisPage();
+    var pageName = thisPage(baseurl);
 
     $('ul.nav a').filter(function() {
         pageName = baseurl + pageName
         $('ul.nav a[href="'+ pageName +'"]').parent().addClass('active');
     });
 
-
 });
 
-function thisPage(){
+/*have the current url page name*/
+function thisPage(baseurl){
     pageName = window.location.href.split(baseurl);
+    
     pageName = pageName[1].split('/');
     pageName = pageName[0];
 
